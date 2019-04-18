e8a5ffb19bfd70656a10b1a8ab5bcb99d1c986c5
Abdoulaye Ndoye <layendoyesn@gmail.com>
2019-04-18 15:38:44 +0000

    v

--- js/monjs.js
+++ js/monjs.js
@@ -1,3 +1,12 @@
-$('.carousel').carousel({
-    interval: 2000
-})
+var navCoul=document.getElementById("coul-transp");
+window.addEventListener("scroll", changerCoul);
+
+function changerCoul(){
+
+    if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1){
+        navCoul.style.backgroundColor="#f8f9fa";
+    }
+    else{
+        navCoul.style.backgroundColor="rgba(0,0,0, 0)";
+    }
+}
